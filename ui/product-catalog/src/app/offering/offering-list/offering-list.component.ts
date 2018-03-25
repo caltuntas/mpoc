06ec4eb156a50cc3a41f8460822e41c3548a0530
sbguneri <sburakguneri@gmail.com>
2018-03-25 15:45:49 +0300

    removed unneccesary methods

--- ui/product-catalog/src/app/offering/offering-list/offering-list.component.ts
+++ ui/product-catalog/src/app/offering/offering-list/offering-list.component.ts
@@ -63,16 +63,10 @@ export class OfferingListComponent implements OnInit {
     ngOnInit() {
 
     }
-
-    onDelete() {
-        console.log("on delete");
-    }
-
     private handleError(error: any) {
         let errMsg = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
         console.error(errMsg); // log to console instead
         return Observable.throw(errMsg);
     }
-
 }
