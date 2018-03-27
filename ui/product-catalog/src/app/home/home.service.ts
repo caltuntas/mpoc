be7eca0963c8397dff6838c8683b3dd633c01951
fahri asi <fahri.aksi@ericsson.com>
2018-03-27 09:19:39 +0300

    no message

---
+++ ui/product-catalog/src/app/home/home.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { HttpClientProvider } from "../shared/httpclientprovider/http-client-provider";
+import { HomeChartsData } from './model/homeChartData';
+
+@Injectable()
+export class HomeService {
+
+    constructor(private http: HttpClientProvider) { }
+
+    getChartsData(): Observable<HomeChartsData[]> {
+        return this.http.get(`/home/getChartsData`);
+    }
+
+}
