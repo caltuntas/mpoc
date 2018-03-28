5c32462faa80e2e3aa4374b43f299582259f2ea6
sbguneri <sburakguneri@gmail.com>
2018-03-28 11:01:05 +0300

    offering create page deleted

--- ui/product-catalog/src/app/offering/offering.module.ts
+++ ui/product-catalog/src/app/offering/offering.module.ts
@@ -7,7 +7,6 @@ import {SmartadminInputModule} from "../shared/forms/input/smartadmin-input.modu
 import {SmartadminValidationModule} from "../shared/forms/validation/smartadmin-validation.module";
 import {SmartadminDatatableModule} from "../shared/ui/datatable/smartadmin-datatable.module";
 import { OfferingListComponent } from './offering-list/offering-list.component';
-import { OfferingCreateComponent } from './offering-create/offering-create.component';
 import { OfferingEditComponent } from './offering-edit/offering-edit.component';
 
 @NgModule({
@@ -19,7 +18,7 @@ import { OfferingEditComponent } from './offering-edit/offering-edit.component';
         SmartadminValidationModule,
         SmartadminInputModule
     ],
-    declarations: [OfferingListComponent, OfferingCreateComponent, OfferingEditComponent]
+    declarations: [OfferingListComponent, OfferingEditComponent]
 })
 export class OfferingModule {
 }
