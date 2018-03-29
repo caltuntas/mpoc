e1674d5312e5d0ac379420df5e4d036e3c18ca82
sbguneri <sburakguneri@gmail.com>
2018-03-29 08:13:54 +0300

    char value use ui developments

--- ui/product-catalog/src/app/characteristic/model/prod-spec-char-value-list.model.ts
+++ ui/product-catalog/src/app/characteristic/model/prod-spec-char-value-list.model.ts
@@ -2,6 +2,4 @@ export class ProdSpecCharValueListModel {
     public prodSpecCharValueUseId: number;
     public prodSpecCharValueId: number;
     public prodSpecCharValue: string;
-    public prodSpecCharId: number;
-    public prodSpecCharDescription: string;
 }
