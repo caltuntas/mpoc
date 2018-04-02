a52a64709592c7f22b3966a5de74be92ffbf9f5a
Seyfullah Tıkıç <stikic@gmail.com>
2018-04-02 10:24:20 +0300

    Product Offering Type Save

--- services/product-catalog/src/main/java/com/ericsson/modernization/services/productcatalog/applicationservice/productoffering/ProductOfferingAppService.java
+++ services/product-catalog/src/main/java/com/ericsson/modernization/services/productcatalog/applicationservice/productoffering/ProductOfferingAppService.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.ericsson.modernization.services.productcatalog.applicationservice.category.CategoryService;
-import com.ericsson.modernization.services.productcatalog.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ericsson.modernization.services.productcatalog.applicationservice.catalog.CatalogAppService;
+import com.ericsson.modernization.services.productcatalog.applicationservice.category.CategoryService;
 import com.ericsson.modernization.services.productcatalog.applicationservice.productoffering.request.ProductOfferingDetailModel;
 import com.ericsson.modernization.services.productcatalog.applicationservice.productoffering.response.ProductOfferingListModel;
 import com.ericsson.modernization.services.productcatalog.applicationservice.productspecification.ProductSpecificationAppService;
+import com.ericsson.modernization.services.productcatalog.model.Catalog;
+import com.ericsson.modernization.services.productcatalog.model.Category;
+import com.ericsson.modernization.services.productcatalog.model.Duration;
+import com.ericsson.modernization.services.productcatalog.model.ProductOffering;
+import com.ericsson.modernization.services.productcatalog.model.ProductOfferingType;
+import com.ericsson.modernization.services.productcatalog.model.ProductSpecification;
 import com.ericsson.modernization.services.productcatalog.repository.ProductOfferingRepository;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import com.ericsson.modernization.services.productcatalog.repository.ProductOfferingTypeRepository;
 
 @Transactional
 @Service
@@ -30,7 +33,9 @@ public class ProductOfferingAppService {
 	@Autowired
 	private CatalogAppService catalogAppService;
 	@Autowired
-    private CategoryService categoryService;
+	private CategoryService categoryService;
+	@Autowired
+	private ProductOfferingTypeRepository productOfferingTypeRepository;
 
 	public ProductOffering create(ProductOfferingDetailModel createRequest) {
 
@@ -40,14 +45,14 @@ public class ProductOfferingAppService {
 		{
 			List<ProductOffering> relatedProductOfferings = new ArrayList<ProductOffering>();
 			for (Integer id : createRequest.getProductOfferingIds()) {
-				//BoChild kaydet
+				// BoChild kaydet
 				ProductOffering childproductOffering = productOfferingRepository.findByIdAndIsDeletedIsFalse(id);
-				//saveFields(childproductOffering, item);
-				//Relation ata
+				// saveFields(childproductOffering, item);
+				// Relation ata
 				relatedProductOfferings.add(childproductOffering);
-				//TODO : BoChild'ın ilişkilerini kaydet
+				// TODO : BoChild'ın ilişkilerini kaydet
 			}
-			//Relation kaydet
+			// Relation kaydet
 			productOffering.setRelatedProductOfferings(relatedProductOfferings);
 			productOfferingRepository.save(productOffering);
 		}
@@ -95,6 +100,11 @@ public class ProductOfferingAppService {
 		productOffering.setSegments(detailModel.getSegments());
 		productOffering.setDocuments(detailModel.getDocuments());
 
+		Integer productOfferingTypeId = 1;
+		if (detailModel.getProductOfferingTypeId() > 0)
+			productOfferingTypeId = detailModel.getProductOfferingTypeId();
+		ProductOfferingType productOfferingType = productOfferingTypeRepository.findById(productOfferingTypeId).get();
+		productOffering.setProductOfferingType(productOfferingType);
 		productOfferingRepository.save(productOffering);
 	}
 
@@ -110,62 +120,45 @@ public class ProductOfferingAppService {
 		return productOfferingRepository.findByIdAndIsDeletedIsFalse(id);
 	}
 
-    public ProductOfferingDetailModel findByIdForEditing(int id) {
-        ProductOffering productOffering = findById(id);
-        return new ProductOfferingDetailModel(
-                productOffering.getId(),
-                productOffering.getName(),
-                productOffering.getDescription(),
-                productOffering.getIsReplicated(),
-                productOffering.getIsSellable(),
-                productOffering.getProductSpecification().getId(),
-                productOffering.getCatalog().getId(),
-                productOffering.getCategory().getId(),
-                productOffering.getProductOfferingType().getId() ,
-                //productOffering.getRelatedProductOfferings());
-                new ArrayList<Integer>());
-    }
-
-    public List<ProductOfferingListModel> findAll() {
-        return productOfferingRepository.findAllByIsDeletedIsFalse().stream()
-                .map(x -> new ProductOfferingListModel(
-                        x.getId(),
-                        x.getName(),
-                        x.getDescription(),
-                        x.getProductSpecification() != null ? x.getProductSpecification().getCode() : null,
-                        x.getCatalog() != null ? x.getCatalog().getName() : null,
-                        x.getCategory() != null ? x.getCategory().getCode() : null,
-                        x.getIsReplicated(),
-                        x.getIsSellable(),
-                        x.getValidFor() != null ? x.getValidFor().getValidForStartDate() : null,
-                        x.getValidFor() != null ? x.getValidFor().getValidForEndDate() : null,
-                        x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodValue() : 0,
-                        x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodUnit() : 0,
-                        x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodValue() : 0,
-                        x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodUnit() : 0,
-                        x.getProductOfferingType() != null ? x.getProductOfferingType().getName(): null)
-                ).collect(Collectors.toList());
-    }
-    
-    public List<ProductOfferingListModel> findAllByProductOfferingTypeId(int productOfferingTypeId) {
-        return productOfferingRepository.findAllByProductOfferingTypeId(productOfferingTypeId).stream()
-                .map(x -> new ProductOfferingListModel(
-                        x.getId(),
-                        x.getName(),
-                        x.getDescription(),
-                        x.getProductSpecification() != null ? x.getProductSpecification().getCode() : null,
-                        x.getCatalog() != null ? x.getCatalog().getName() : null,
-                        x.getCategory() != null ? x.getCategory().getCode() : null,
-                        x.getIsReplicated(),
-                        x.getIsSellable(),
-                        x.getValidFor() != null ? x.getValidFor().getValidForStartDate() : null,
-                        x.getValidFor() != null ? x.getValidFor().getValidForEndDate() : null,
-                        x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodValue() : 0,
-                        x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodUnit() : 0,
-                        x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodValue() : 0,
-                        x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodUnit() : 0,
-                        x.getProductOfferingType() != null ? x.getProductOfferingType().getName(): null)
-                ).collect(Collectors.toList());
-    }
+	public ProductOfferingDetailModel findByIdForEditing(int id) {
+		ProductOffering productOffering = findById(id);
+		return new ProductOfferingDetailModel(productOffering.getId(), productOffering.getName(),
+				productOffering.getDescription(), productOffering.getIsReplicated(), productOffering.getIsSellable(),
+				productOffering.getProductSpecification().getId(), productOffering.getCatalog().getId(),
+				productOffering.getCategory().getId(), productOffering.getProductOfferingType().getId(),
+				// productOffering.getRelatedProductOfferings());
+				new ArrayList<Integer>());
 	}
-	
+
+	public List<ProductOfferingListModel> findAll() {
+		return productOfferingRepository.findAllByIsDeletedIsFalse().stream()
+				.map(x -> new ProductOfferingListModel(x.getId(), x.getName(), x.getDescription(),
+						x.getProductSpecification() != null ? x.getProductSpecification().getCode() : null,
+						x.getCatalog() != null ? x.getCatalog().getName() : null,
+						x.getCategory() != null ? x.getCategory().getCode() : null, x.getIsReplicated(),
+						x.getIsSellable(), x.getValidFor() != null ? x.getValidFor().getValidForStartDate() : null,
+						x.getValidFor() != null ? x.getValidFor().getValidForEndDate() : null,
+						x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodValue() : 0,
+						x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodUnit() : 0,
+						x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodValue() : 0,
+						x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodUnit() : 0,
+						x.getProductOfferingType() != null ? x.getProductOfferingType().getName() : null))
+				.collect(Collectors.toList());
+	}
+
+	public List<ProductOfferingListModel> findAllByProductOfferingTypeId(int productOfferingTypeId) {
+		return productOfferingRepository.findAllByProductOfferingTypeId(productOfferingTypeId).stream()
+				.map(x -> new ProductOfferingListModel(x.getId(), x.getName(), x.getDescription(),
+						x.getProductSpecification() != null ? x.getProductSpecification().getCode() : null,
+						x.getCatalog() != null ? x.getCatalog().getName() : null,
+						x.getCategory() != null ? x.getCategory().getCode() : null, x.getIsReplicated(),
+						x.getIsSellable(), x.getValidFor() != null ? x.getValidFor().getValidForStartDate() : null,
+						x.getValidFor() != null ? x.getValidFor().getValidForEndDate() : null,
+						x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodValue() : 0,
+						x.getWarrantyPeriod() != null ? x.getWarrantyPeriod().getPeriodUnit() : 0,
+						x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodValue() : 0,
+						x.getReturnPeriod() != null ? x.getReturnPeriod().getPeriodUnit() : 0,
+						x.getProductOfferingType() != null ? x.getProductOfferingType().getName() : null))
+				.collect(Collectors.toList());
+	}
+}
