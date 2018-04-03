8645d73d378ccba8845f33fc3bb50c75126b1b5f
Yakup Buğra Bozkurt <yakup.bugra.bozkurt@gmail.com>
2018-04-03 15:31:20 +0300

    bundle components

--- ui/product-catalog/src/app/bundle/bundle-edit/bundle-offerings/bundle-offerings.component.ts
+++ ui/product-catalog/src/app/bundle/bundle-edit/bundle-offerings/bundle-offerings.component.ts
@@ -1,43 +1,48 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectorRef,
+  OnDestroy
+} from "@angular/core";
 import { BundleModel } from "../../model/bundle-model";
 import { BundleService } from "../../bundle.service";
 import { IdNameDescriptionModel } from "../../model/idNameDescriptionModel";
-
+import { Response } from "@angular/http";
 
 @Component({
   selector: "app-bundle-offerings",
   templateUrl: "./bundle-offerings.component.html"
 })
 export class BundleOfferingsComponent implements OnInit {
-  
-  simpleOfferings:Array<IdNameDescriptionModel>;
+  offerings: Array<IdNameDescriptionModel>;
+  reRenderTable = false;
 
   @Input() model: BundleModel;
+
   constructor(
-    private bundleService:BundleService
-   
-  ) {
-   
+    private bundleService: BundleService,
+    private cdRef: ChangeDetectorRef
+  ) {}
+
+  async ngOnInit() {
+    this.model.simpleProductOfferingIds = [];
+    // let data = await this.bundleService.getSimpleOfferingsForSelect().toPromise<IdNameDescriptionModel[]>();
+    // this.offerings=data;
+    // console.log(this.offerings);
   }
-
-
-  ngOnInit() {
-    this.bundleService.getSimgpleOfferingsForSelect().subscribe(data=>this.simpleOfferings=data);
-
-  }
+  
 
 
 
   dtOptions = {
     dom: "Bfrtip",
     ajax: (data, callback, settings) => {
-      this.bundleService
-        .getSimgpleOfferingsForSelect()       
-        .subscribe(data => {
-          callback({
-            aaData: data
-          });
+      this.bundleService.getSimpleOfferingsForSelect().subscribe(data => {
+        callback({
+          aaData: data
         });
+      });
     },
     columns: [
       { data: "id" },
@@ -45,13 +50,51 @@ export class BundleOfferingsComponent implements OnInit {
       { data: "description" },
       {
         render: (data, type, fullRow, meta) => {
-          return "<button class='btn btn-info btn-sm pull-right'  offering-id='${fullRow.id}'><i class='fa fa-fw fa-plus'></i></button>";
+          return `<a class='btn btn-success btn-sm pull-right sa-datatables-add-offering'  offering-id='${
+            fullRow.id
+          }'><i class='fa fa-fw fa-plus'></i></a>`;
         }
       }
     ],
     order: [[0, "desc"]]
   };
 
+  ngAfterViewInit() {
+    document.querySelector("body").addEventListener("click", event => {
+      let target = <Element>event.target;
 
+      if (
+        target.tagName.toLowerCase() === "a" &&
+        jQuery(target).hasClass("sa-datatables-add-offering")
+      ) {
+        this.onAddOffering(target.getAttribute("offering-id"));
+      }
+    });
+    // console.log(this.offerings);
+  }
 
+  onAddOffering(offeringId) {
+    var element = jQuery("a[offering-id='" + offeringId + "']");
+
+    if (element.hasClass("btn-success")) {
+      element.removeClass("btn-success");
+      element.addClass("btn-danger");
+      element.html("<i class='fa fa-fw fa-minus'></i>");
+      this.model.simpleProductOfferingIds.push(offeringId);
+    } else {
+      element.removeClass("btn-danger");
+      element.addClass("btn-success");
+      element.html("<i class='fa fa-fw fa-plus'></i>");
+      this.model.simpleProductOfferingIds = this.model.simpleProductOfferingIds.filter(
+        x => x != offeringId
+      );
+    }
+  }
+  reloadSpecListTable() {
+    this.reRenderTable = true;
+    if (!this.cdRef["destroyed"]) {
+      this.cdRef.detectChanges();
+    }
+    this.reRenderTable = false;
+  }
 }
