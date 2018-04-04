0bdaa69b9e7d5e4a391d0379b58395adfa8066c3
Yakup Buğra Bozkurt <yakup.bugra.bozkurt@gmail.com>
2018-04-04 08:23:40 +0300

    bundle

--- ui/product-catalog/src/app/bundle/bundle-edit/bundle-edit.component.ts
+++ ui/product-catalog/src/app/bundle/bundle-edit/bundle-edit.component.ts
@@ -4,6 +4,7 @@ import { SalesChannel } from "../../sales-channel/detail/sales-channel";
 import { BundleModel } from "../model/bundle-model";
 import { CharacteristicService } from "../../characteristic/characteristic.service";
 import { ProdSpecCharValueUseListModel } from "../../characteristic/model/prod-spec-char-value-use-list.model";
+import { BundleService } from "../bundle.service";
 @Component({
   selector: "app-bundle-edit",
   templateUrl: "./bundle-edit.component.html"
@@ -13,15 +14,15 @@ export class BundleEditComponent implements OnInit {
   charValueUseList: Array<ProdSpecCharValueUseListModel> = [];
   constructor(
     private router: Router,
-    private route: ActivatedRoute  
+    private route: ActivatedRoute,
+    private bundleService: BundleService
   ) {
     this.model = new BundleModel();
     const idParam = route.snapshot.params.offeringId;
     if (idParam) {
       this.model.id = idParam;
       this.model.isNewbundle = false;
-    }
-    else{
+    } else {
       this.model.isNewbundle = true;
     }
   }
@@ -31,9 +32,6 @@ export class BundleEditComponent implements OnInit {
   ngAfterViewInit() {
     var self = this;
 
-    
-
-  
     //Wizard Events
     jQuery("#offeringWizard").on("actionclicked.fu.wizard", function(
       event,
@@ -44,7 +42,6 @@ export class BundleEditComponent implements OnInit {
       //     event.preventDefault();
       //   }
       // }
-
       // console.log(data.direction);
       // console.log(data.step);
       // console.log(data);
@@ -53,24 +50,25 @@ export class BundleEditComponent implements OnInit {
   }
 
   onWizardComplete(data) {
-    console.log("fuel-ux wizard complete", data);
+    //console.log("fuel-ux wizard complete", data);
     // this.model.salesChannels = this.selectedSalesChannels;
     // this.model.segments = this.selectedSegments;
     // this.model.documents = this.selectedDocuments;
-    // this.model.productOfferingTypeId = 2;
+    this.model.productOfferingTypeId = 2;
     // this.model.simpleProductOfferingIds = [];
     // this.model.simpleProductOfferingIds.push(336);
     // this.model.simpleProductOfferingIds.push(337);
+    console.log(this.model);
 
-    // console.log(this.isNewOffering);
-    // if (this.isNewOffering) {
-    //     this.bundleService.createOffering(this.model).subscribe(data => {
-    //         this.router.navigate(['/bundle/bundle-list']);
-    //     });
-    // } else {
+    if (this.model.isNewbundle) {
+      this.bundleService.createOffering(this.model).subscribe(data => {
+        this.router.navigate(["/bundle/bundle-list"]);
+      });
+    }
+    // else {
     //     this.bundleService.updateOffering(this.model).subscribe(data => {
     //         this.router.navigate(['/bundle/bundle-list']);
     //     });
-    // }
+    //}
   }
 }
